372d6fd3e7c188718d1d8788b5d83a288516a672
Ankur Gangwar <ankur999gangwar@gmail.com>
2025-05-23 15:54:15 +0530

    Version 1.0.0 ready

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	api "github.com/Sheldon1999/goBank/api"
+	db "github.com/Sheldon1999/goBank/db/sqlc"
+	util "github.com/Sheldon1999/goBank/util"
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
 }
